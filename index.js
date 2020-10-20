f1e9638a681c99fa986aa5d993f066c551b48609
ArielPenna <arielpenna@gmail.com>
2020-10-20 18:21:45 -0300

    Added Likes, Comments, comments and like counters and a subscription function, BACK END FINISHED

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 require('dotenv').config();
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, PubSub } = require('apollo-server');
 const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const { MONGODB } = process.env;
 
+const pubsub = new PubSub();
+
 //Server Configuration
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => ({ req }),
+  context: ({ req }) => ({ req, pubsub }),
 });
 
 //MongoDB Conection
