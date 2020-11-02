6acd893d28f32eabf87a0805b52e65ba4b04b7fe
ArielPenna <arielpenna@gmail.com>
2020-11-02 16:15:30 -0300

    single post finished but not working

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 
 import { AuthContext } from '../context/auth';
 import LikeButton from './LikeButton';
+import DeleteButton from './DeleteButton';
 
 function PostCard({ post: { body, createdAt, id, username, likeCount, commentCount, likes } }) {
   const { user } = useContext(AuthContext);
@@ -34,11 +35,7 @@ function PostCard({ post: { body, createdAt, id, username, likeCount, commentCou
             {commentCount}
           </Label>
         </Button>
-        {user && user.username === username && (
-          <Button as="div" color="grey" floated="right" onClick={() => console.log('Delete post')}>
-            <Icon name="trash" style={{ margin: 0 }} />
-          </Button>
-        )}
+        {user && user.username === username && <DeleteButton postId={id} />}
       </Card.Content>
     </Card>
   );
